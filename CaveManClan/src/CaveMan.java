05231df0fdb0ff30f4ed4072b3172de57c7cac5c
Nathan Sodja Obodai <sodjanathan@gmail.com>
2022-01-14 14:47:45 +0000

    added rockCollection property to CaveMan These cavemen collect rocks

--- CaveManClan/src/CaveMan.java
+++ CaveManClan/src/CaveMan.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class CaveMan {
 
 	// getters and setters,
@@ -5,19 +7,22 @@ public class CaveMan {
 	private String name;
 	private int age;
 	private float weight;
+	private ArrayList<Rock> rockCollection;
 	
 	// parameterised constructor
-	public CaveMan(String n, int a, float w) {
+	public CaveMan(String n, int a, float w, ArrayList<Rock> r) {
 		this.name = n;
 		this.age = a;
 		this.weight = w;
+		this.rockCollection = r;
 	}
 
 	// default constructor
 	public CaveMan() {
 		this.name="They haven't given me a name yet";
 		this.age=0;
-		this.weight=100f;
+		this.weight = 100f;
+		this.rockCollection = new ArrayList<>();
 	}
 
 	public String getName() {
@@ -45,7 +50,19 @@ public class CaveMan {
 	}
 
 	public void SayHi() {
-		System.out.println("Hi, my name is " + this.name );
+		System.out.println("Hi, my name is " + this.name);
 	}
 
+	public ArrayList<Rock> getRockCollection() {
+		return rockCollection;
+	}
+
+	public void setRockCollection(ArrayList<Rock> rockCollection) {
+		this.rockCollection = rockCollection;
+	}
+	
+	@Override
+	public String toString() {
+		return "CaveMan [age=" + age + ", name=" + name + ", weight=" + weight + "]";
+	}
 }
